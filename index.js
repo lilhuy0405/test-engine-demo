fbc7633c20d957c4ce2677412ed933816d6cd7c3
lilhuy0405 <luuhuy040501.travail@gmail.com>
2024-05-04 01:26:22 +0700

    update  test

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ test('My first test', async t => {
     await t.expect(1).eql(1);
 });
 
-test('Fail test', async t => {
-    //this test will fail
-   await t.expect(1).eql(2);
-});
+// test('Fail test', async t => {
+//     //this test will fail
+//    await t.expect(1).eql(2);
+// });
 
 test('My second test', async t => {
     //try to search for "testcafe" in google
